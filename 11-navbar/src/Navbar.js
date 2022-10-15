e046e411b8f2b2a27ccbdc18e3af568850fb6245
Amélie Monnoyer <49373560+HungryOctopus@users.noreply.github.com>
2022-10-15 15:08:11 +0200

    test

--- 11-navbar/src/Navbar.js
+++ 11-navbar/src/Navbar.js
@@ -17,11 +17,7 @@ const Navbar = () => {
             <FaBars />
           </button>
         </div>
-        <div
-          className={`${
-            showLinks ? 'links-container show-container' : 'links-container'
-          }`}
-        >
+        <div className="links-container">
           <ul className="links">
             {links.map((link) => {
               const { id, url, text } = link;
